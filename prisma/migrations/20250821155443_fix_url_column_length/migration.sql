4ce6ea368600987830709099ea7d22884b61f55c
unknown <ron.hillel.business@gmail.com>
2025-08-21 15:55:36 +0300

    fixed url not long enough

---
+++ prisma/migrations/20250821155443_fix_url_column_length/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `SearchConsoleKeywordDailyStat` MODIFY `topRankingPageUrl` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `SearchConsoleKeywordMonthlyStat` MODIFY `topRankingPageUrl` TEXT NOT NULL;
